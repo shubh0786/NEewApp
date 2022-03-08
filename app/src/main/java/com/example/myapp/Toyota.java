b8aa4b6a5845dc40755d0fe5f89667ebe894731b
SHUBHAM SINGH <shubham.singh267@gmail.com>
2022-03-09 03:28:54 +1300

    Dashboard

---
+++ app/src/main/java/com/example/myapp/Toyota.java
@@ -0,0 +1,14 @@
+package com.example.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Toyota extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_toyota);
+    }
+}
