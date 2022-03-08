bcde3fbddee7a531b63cf8221e1c10679498c17d
SHUBHAM SINGH <shubham.singh267@gmail.com>
2022-03-09 03:27:00 +1300

    Dashboard

--- app/src/main/java/com/example/myapp/MainHome.java
+++ app/src/main/java/com/example/myapp/MainHome.java
@@ -6,16 +6,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainHome extends AppCompatActivity {
 
-
+ Button btnToyo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_home);
 
+         btnToyo = findViewById(R.id.btnToyo);
+         btnToyo.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+
+                 Intent intent = new Intent(getApplicationContext(),Toyota.class);
+                 startActivity(intent);
+
+                 Toast.makeText(MainHome.this, "Welcome to Auckland Toyota", Toast.LENGTH_SHORT).show();
+             }
+         });
 
 
     }
