b602862731e1e2652f730c114c0af36ff7d1387b
SHUBHAM SINGH <shubham.singh267@gmail.com>
2022-03-11 04:08:18 +1300

    sms Function

--- app/src/main/java/com/example/myapp/MainActivity.java
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -4,31 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnMain;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnMain = (Button) findViewById(R.id.btnMain);
-
-        btnMain.setOnClickListener(new View.OnClickListener() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
             @Override
-            public void onClick(View view) {
-
-                Intent intent = new Intent(getApplicationContext(),mainScreen.class);
-                startActivity(intent);
-
-                Toast.makeText(MainActivity.this, "Here You can Sign In and Sign Up", Toast.LENGTH_SHORT).show();
+            public void run() {
+                startActivity(new Intent(MainActivity.this,mainScreen.class));
+                finish();
             }
-        });
+        }, 2000);
 
 
     }
