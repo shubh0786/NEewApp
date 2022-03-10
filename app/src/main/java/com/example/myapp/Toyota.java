32a12d351d26de064926c6bd6f0db6d2674a0102
SHUBHAM SINGH <shubham.singh267@gmail.com>
2022-03-11 04:08:28 +1300

    sms Function

--- app/src/main/java/com/example/myapp/Toyota.java
+++ app/src/main/java/com/example/myapp/Toyota.java
@@ -2,13 +2,52 @@ package com.example.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class Toyota extends AppCompatActivity {
 
+
+   ImageView imgCall;
+    ImageView imgSms;
+    ImageView btnBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_toyota);
+
+        btnBack = findViewById(R.id.btnBack);
+      imgCall = findViewById(R.id.imgCall);
+        imgSms = findViewById(R.id.imgSms);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentBack = new Intent(getApplicationContext(),MainHome.class);
+                startActivity(intentBack);
+            }
+        });
+        imgCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:02041248991"));
+                startActivity(intent);
+            }
+        });
+
+        imgSms.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentsms = new Intent(Intent.ACTION_VIEW, Uri.fromParts("sms","02041248991", null));
+                intentsms.putExtra("sms_body", "Hello Sir");
+                startActivity(intentsms);
+            }
+        });
     }
 }
