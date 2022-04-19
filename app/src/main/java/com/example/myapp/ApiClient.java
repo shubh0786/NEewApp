5dddfa57f8ee6a50a19828c845f410218ebb5553
SHUBHAM SINGH <shubham.singh267@gmail.com>
2022-04-20 00:41:53 +1200

    Adding new feature of reporting and adding task to perform

---
+++ app/src/main/java/com/example/myapp/ApiClient.java
@@ -0,0 +1,22 @@
+package com.example.myapp;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+public class ApiClient {
+
+    private static final String Base_URL = "https://my-reporting-app.000webhostapp.com/";
+    private static Retrofit retrofit;
+
+    public static Retrofit getApiClient() {
+
+        if (retrofit== null){
+
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(Base_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+
+        return retrofit;
+    }
+}
