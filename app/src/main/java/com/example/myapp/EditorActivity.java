b692e8217530ed4ca8bd4e2a73b0c4c325eeed15
SHUBHAM SINGH <shubham.singh267@gmail.com>
2022-04-20 00:42:14 +1200

    Adding new feature of reporting and adding task to perform

---
+++ app/src/main/java/com/example/myapp/EditorActivity.java
@@ -0,0 +1,132 @@
+package com.example.myapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.thebluealliance.spectrum.SpectrumPalette;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class EditorActivity extends AppCompatActivity {
+
+    EditText et_title, et_note;
+    ProgressDialog progressDialog;
+    SpectrumPalette palette;
+
+    ApiInterface apiInterface;
+
+    int color;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editor);
+
+        et_title = findViewById(R.id.title);
+        et_note = findViewById(R.id.note);
+        palette = findViewById(R.id.palette);
+
+
+        palette.setOnColorSelectedListener(
+                clr -> color = clr
+        );
+
+        //this to make any default color
+        palette.setSelectedColor(-8469267);
+        color = 8469267;
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Please wait...");
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_editor,menu);
+        return true;
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.save:
+
+                String title = et_title.getText().toString().trim();
+                String note = et_note.getText().toString().trim();
+                int color = this.color;
+
+                if (title.isEmpty()) {
+                    et_title.setError("Please Enter Title");
+                } else if (note.isEmpty()) {
+                    et_note.setError("Please Enter note");
+                } else {
+                    saveNote(title, note, color);
+                }
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void saveNote(final String title, final String note, final int  color) {
+
+        progressDialog.show();
+        apiInterface = ApiClient.getApiClient().create(ApiInterface.class);
+        Call<Note> call = apiInterface.saveNote(title,note,color);
+
+        call.enqueue(new Callback<Note>() {
+            @Override
+            public void onResponse(@NonNull Call<Note> call, @NonNull Response<Note> response) {
+
+
+                progressDialog.dismiss();
+
+                if (response.isSuccessful() && response.body() != null){
+
+                    Boolean success = response.body().getSuccess();
+                    if (success){
+
+                        Toast.makeText(EditorActivity.this,
+                                response.body().getMessage(),
+                                Toast.LENGTH_SHORT).show();
+
+                        finish();
+
+
+                    }
+                    else{
+                        Toast.makeText(EditorActivity.this,
+                                response.body().getMessage(),
+                                Toast.LENGTH_SHORT).show();
+
+                    }
+
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<Note> call, @NonNull Throwable t) {
+
+                progressDialog.dismiss();
+                Toast.makeText(EditorActivity.this,
+                        t.getLocalizedMessage(),
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+    }
+}
